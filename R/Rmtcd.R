a2f42ddabd3c1621ddd3b1ad01a77e1e75c0d3b0
Charley <charleycyx@gmail.com>
2015-08-24 11:28:41 -0400

    description file author added, date vacancy filled

--- R/Rmtcd.R
+++ R/Rmtcd.R
@@ -27,7 +27,7 @@
 #' \tabular{ll}{
 #' Package: \tab Rmtcd\cr
 #' Version: \tab 0.1\cr
-#' Date: \tab [Put date here] \cr
+#' Date: \tab Aug 24 2015 \cr
 #' License: \tab This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. To view a copy of this license, visit \url{http://creativecommons.org/licenses/by-nc-sa/4.0/} or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.\cr
 #' }
 #' 
